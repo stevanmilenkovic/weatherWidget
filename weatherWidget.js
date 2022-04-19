ee8fa49a0a15c08a9ba9522b443497b48efc9f20
stevix00 <stevanmilenkovic1337@gmail.com>
2022-04-19 21:20:11 +0200

    editujem ja editujes ti

--- weatherWidget.js
+++ weatherWidget.js
@@ -1,5 +1,8 @@
 const apiKey = '46060088bca4f2b154df885f01ea557c';
 
+let temperature = 0;
+let windspeed = 0;
+
 function fetchWeather(city){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=metric&appid='+ apiKey)
             .then(resp => resp.json())
@@ -13,10 +16,12 @@ function displayWeather(data){
         const { speed} = data.wind;
         document.getElementById('cityname').innerHTML = name;
         document.querySelector('.temp').innerHTML = `${temp}°C`;
+        temperature = temp;
         document.querySelector('.icon').src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
         document.querySelector('.description').innerHTML = description;
-        document.querySelector('.humidity').innerHTML = 'Humidity: ' + humidity;
+        document.querySelector('.humidity').innerHTML = 'Humidity: ' + humidity + '%';
         document.querySelector('.wind').innerHTML = 'Wind Speed: ' + speed + 'km/h';
+        windspeed = speed;
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name + "')";
     }
 
@@ -34,10 +39,23 @@ input.addEventListener('keydown', (e)=>{
     if (e.key == 'Enter'){
         document.querySelector('.weather').classList.remove('hide');
         fetchWeather(input.value);
-
+        input.value = '';
     };
+});
 
-})
+let switchCount = 0;
+
+document.getElementById('switch').onclick = () =>{
+    if (switchCount == 0){
+        document.querySelector('.temp').innerHTML = `${Math.round(100*(temperature*9/5+32))/100}°F`;
+        document.querySelector('.wind').innerHTML = 'Wind Speed: ' + Math.floor(100*windspeed/1.609344)/100 + 'mph';
+        switchCount+=1;
+    } else {
+        document.querySelector('.temp').innerHTML = `${temperature}°C`;
+        document.querySelector('.wind').innerHTML = 'Wind Speed: ' + windspeed + 'km/h';
+        switchCount-=1;
+    }   
+}
 
 
 
